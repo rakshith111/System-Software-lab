1f5f49cd6b90878f3a3e322951dc66e1d5fbb326
rakshith111 <rakshithbabu111@gmail.com>
2022-07-23 01:57:44 +0530

    Update program

--- 9lab.c
+++ 9lab.c
@@ -5,85 +5,89 @@ void lru(char[], char[], int, int);
 void opt(char[], char[], int, int);
 int main()
 {
-    int ch, YN = 1, i, l, f;
-    char F[10], s[25];
+    int ch, YN = 1, i, length_of_string, frame_length;
+    char FRAMES[10], s[25];
     printf("\n\n\tEnter the no ofempty frames: ");
-    scanf("%d", &f);
+    scanf("%d", &frame_length);
     printf("\n\n\tEnter the length of the string: ");
-    scanf("%d", &l);
+    scanf("%d", &length_of_string);
     printf("\n\n\tEnter the string: ");
     scanf("%s", s);
-    for (i = 0; i < f; i++)
-        F[i] = -1;
-    do
+
+    for (i = 0; i < frame_length; i++)
+    {
+        FRAMES[i] = "#";
+    }
+    for (;;)
     {
         printf("\n\n\t*********** MENU ***********");
         printf("\n\n\t1:FIFO\n\n\t2:LRU\n\n\t4:EXIT");
-        printf("\n\n\tEnter your choice:");
+        printf("\n\n\tEnter your choice: ");
         scanf("%d", &ch);
         switch (ch)
         {
         case 1:
-            for (i = 0; i < f; i++)
-            {
-                F[i] = -1;
-            }
-            FIFO(s, F, l, f);
+
+            FIFO(s, FRAMES, length_of_string, frame_length);
             break;
         case 2:
-            for (i = 0; i < f; i++)
-            {
-                F[i] = -1;
-            }
-            lru(s, F, l, f);
+
+            lru(s, FRAMES, length_of_string, frame_length);
             break;
         case 4:
             exit(0);
         }
         printf("\n\n\tDo u want to continue IF YES PRESS 1\n\n\tIF NO PRESS 0 :");
         scanf("%d", &YN);
-    } while (YN == 1);
+    }
     return (0);
 }
 // FIFO
-void FIFO(char s[], char F[], int l, int f)
+// i is main var, j in second var,k is counter var
+void FIFO(char input[], char FRAMES[], int length_of_string, int frame_length)
 {
-    int i, j = 0, k, flag = 0, cnt = 0;
-    printf("\n\tPAGE\t FRAMES\t FAULTS");
-    for (i = 0; i < l; i++)
+    int i, k = 0, j, flag = 0, cnt = 1;
+    printf("\n\tPAGE\t FRAMES \t FAULTS");
+    for (i = 0; i < length_of_string; i++)
     {
-        for (k = 0; k < f; k++)
+        for (j = 0; j < frame_length; j++)
         {
-            if (F[k] == s[i])
+            // check if the page is in the frames
+            if (FRAMES[j] == input[i])
+            {
                 flag = 1;
+                break;
+            }
         }
+        printf("\n\t%c\t", input[i]);
         if (flag == 0)
         {
-            printf("\n\t%c\t", s[i]);
-            F[j] = s[i];
-            j++;
-            for (k = 0; k < f; k++)
+            // add the page to the frames
+            FRAMES[k] = input[i];
+            k++;
+            for (j = 0; j < frame_length; j++)
             {
-                printf(" %c", F[k]);
+                printf(" %c", FRAMES[j]);
             }
-            printf("\tPage-fault%d", cnt);
+            printf("\tPage-fault %d", cnt);
             cnt++;
         }
         else
         {
             flag = 0;
-            printf("\n\t%c\t", s[i]);
-            for (k = 0; k < f; k++)
+
+            for (j = 0; j < frame_length; j++)
             {
-                printf(" %c", F[k]);
+                printf(" %c", FRAMES[j]);
             }
             printf("\tNo page-fault");
         }
-        if (j == f)
-            j = 0;
+        // in case the frame is full, the first element is removed and the new element is added to the end of the frame,by reseting k to 0
+        if (k == frame_length)
+            k = 0;
     }
 }
-// LRU
+// LRU #unchanged
 void lru(char s[], char F[], int l, int f)
 {
     int i, j = 0, k, m, flag = 0, cnt = 0, top = 0;
